27eb5c39634b14f9cf1657b8ff20870968485fde
Adam Fortuna <adam@fortuna.name>
2016-11-27 00:29:00 -0500

    Cleaning up css

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -5,10 +5,8 @@ class Seed
     @data = Hashie::Mash.new
     Dir['data/*.yml'].each do |path|
       name = File.basename(path).split('.').first
-      # binding.pry if name == 'featured_posts'
 
       hash = Hashie::Extensions::Parsers::YamlErbParser.perform(path)
-
 
       @data.assign_property(name, hash)
     end
